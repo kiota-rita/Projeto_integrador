b1505ad6831560041d9b687d23daf7b0e411adfc
kiota-rita <rita-kiota@hotmail.com>
2021-04-30 12:36:06 -0300

    task3

---
+++ projeto/repassa/src/main/java/com/doare/repassa/RepassaApplication.java
@@ -0,0 +1,13 @@
+package com.doare.repassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepassaApplication.class, args);
+	}
+
+}
